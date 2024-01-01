149f1a52170c2dc7d23da3ec8c420f1e11faddbb
Toya021 <132384342+Toya021@users.noreply.github.com>
2024-01-01 00:23:57 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+let date = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let p = document.querySelector("#the-day");
+let hrs = date.getHours();
+let min = date.getMinutes();
+let dai = date.getDay();
+p.innerHTML = `${days[dai]} ${hrs}:${min} `;
+
+let search = document.querySelector("#search");
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(".maintemp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+}
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+function city(event) {
+  event.preventDefault();
+  let name = document.querySelector("h1");
+  let town = document.querySelector("#city-input");
+
+  if (town !== "") {
+    searchCity(`${town.value}`);
+  } else {
+    alert("Insert a City name");
+    searchCity("London");
+  }
+}
+search.addEventListener("click", city);
